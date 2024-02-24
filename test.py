1cd9a59316c9133e325dc850943afeb93a444fb3
rtx-3070 <116584229+rtx-3070@users.noreply.github.com>
2024-02-24 17:34:59 +0100

    Refactor dog classes and add new dog instance

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-class Hund():
-    def __init__(self, größe=0, gewicht=0, farbe="schwarz"):
-        self.größe = größe
-        self.gewicht = gewicht
-        self.farbe = farbe
-    def summary(self, Type):
-        print("Größe:", self.größe, "Gewicht:", self.gewicht, "Farbe:", self.farbe, "Hund:", Type)
-        print("Bellen:", self._bellverhalten.__class__.__name__, "Laufen:", self._laufverhalten.__class__.__name__)
-
 class Bellverhalten():
     def bellen(self):
         pass
@@ -34,50 +25,55 @@ class Laufverhalten():
     class SchnellLaufen():
         def laufen(self):
             print("Ich laufe schnell")
+            
 
-class Husky(Hund):
-    def __init__(self, größe=0, gewicht=0, farbe="weiß", bellverhalten = Bellverhalten.NormalBellen(), laufverhalten = Laufverhalten.SchnellLaufen()):
-        Hund.__init__(self, größe, gewicht, farbe)
+class Hund():
+    def __init__(self, type, größe=0, gewicht=0, farbe="schwarz", bellverhalten = Bellverhalten.NormalBellen(), laufverhalten = Laufverhalten.LangsamLaufen()):
+        self._größe = größe
+        self._gewicht = gewicht
+        self._farbe = farbe
         self._bellverhalten = bellverhalten
         self._laufverhalten = laufverhalten
-        Hund.summary(self, "Husky")
+        self._type = type
+        self.summary()
+
+    def summary(self):
+        print("Größe:", self._größe, "Gewicht:", self._gewicht, "Farbe:", self._farbe, "Hund:", self._type)
+        print("Bellen:", self._bellverhalten.__class__.__name__, "Laufen:", self._laufverhalten.__class__.__name__)
+        print("")
 
     def bellen(self):
         self._bellverhalten.bellen() 
-
+    
     def laufen(self):
         self._laufverhalten.laufen()
+
+
+
+class Husky(Hund):
+    def __init__(self, type, größe, gewicht, farbe, bellverhalten, laufverhalten):
+        Hund.__init__(self, type, größe, gewicht, farbe, bellverhalten, laufverhalten)
         
 class Bulldogge(Hund):
-    def __init__(self, größe=0, gewicht=0, farbe="schwarz", bellverhalten = Bellverhalten.LautBellen(), laufverhalten = Laufverhalten.LangsamLaufen()):
-        Hund.__init__(self, größe, gewicht, farbe)
-        self._bellverhalten = bellverhalten
-        self._laufverhalten = laufverhalten
-        Hund.summary(self, "Bulldogge")
-
-    def bellen(self):
-        self._bellverhalten.bellen()
-
-    def laufen(self):
-        self._laufverhalten.laufen()
+    def __init__(self, type, größe, gewicht, farbe, bellverhalten, laufverhalten):
+        Hund.__init__(self, type, größe, gewicht, farbe, bellverhalten, laufverhalten)
 
 class Pudel(Hund):
-    def __init__(self, größe=0, gewicht=0, farbe="braun", bellverhalten = Bellverhalten.LeiseBellen(), laufverhalten = Laufverhalten.LangsamLaufen()):
-        Hund.__init__(self, größe, gewicht, farbe)
-        self._bellverhalten = bellverhalten
-        self._laufverhalten = laufverhalten
-        Hund.summary(self, "Pudel")
+    def __init__(self, type, größe, gewicht, farbe, bellverhalten, laufverhalten):
+        Hund.__init__(self, type, größe, gewicht, farbe, bellverhalten, laufverhalten)
 
-    def bellen(self): 
-        self._bellverhalten.bellen()
-
-    def laufen(self):    
-        self._laufverhalten.laufen()
 
 if __name__ == "__main__":
-    husky = Husky(10, 5, "weiß", Bellverhalten.NormalBellen())
-    bulldogge = Bulldogge(5, 20, "schwarz", Bellverhalten.LautBellen())
-    pudel = Pudel(3, 2, "braun", Bellverhalten.LeiseBellen())
+    husky = Husky("Husky", 10, 5, "weiß", Bellverhalten.NormalBellen(), Laufverhalten.SchnellLaufen())
+    bulldogge = Bulldogge("Bulldogge", 5, 20, "schwarz", Bellverhalten.LautBellen(), Laufverhalten.LangsamLaufen())
+    pudel = Pudel("Pudel", 3, 2, "braun", Bellverhalten.LeiseBellen(), Laufverhalten.SchnellLaufen())
+    newDog = Hund("Labradudel", 5, 10, "schwarz", Bellverhalten.NormalBellen(), Laufverhalten.LangsamLaufen())
+    
+    print("")
+
     husky.bellen()
     bulldogge.bellen()
     pudel.bellen()
+    newDog.bellen()
+
+    newDog.laufen()
